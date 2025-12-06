d61156f7c3471a689cda08f70f12c7e1aac20c06
Daniel Ilievski <dilievski54@gmail.com>
2025-12-07 00:28:28 +0100

    Day 4: Printing Department

---
+++ src/main/java/aoc2025/day04/PrintingDepartment2.java
@@ -0,0 +1,78 @@
+package aoc2025.day04;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PrintingDepartment2 {
+
+  private static final int[][] DIRECTIONS = {
+    {-1, -1}, {-1, 0}, {-1, 1}, // top left, top, top right
+    {0, -1}, {0, 1}, // left, right
+    {1, -1}, {1, 0}, {1, 1} // bottom left, bottom, bottom right
+  };
+
+  public long solution(String filename) {
+
+    InputStream is = getClass().getResourceAsStream(filename);
+    List<String> grid = new BufferedReader(new InputStreamReader(is))
+      .lines()
+      .collect(Collectors.toList());
+
+    int rows = grid.size();
+    int cols = grid.get(0).length();
+
+    long accessiblePapers = 0;
+
+    while (true) {
+      List<Point> removedPapers = new ArrayList<>();
+
+      for (int i = 0; i < rows; i++) {
+        char[] row = grid.get(i).toCharArray();
+
+        for (int j = 0; j < cols; j++) {
+          if (row[j] != '@') continue;
+
+          int neighborPapers = 0;
+
+          for (int[] d : DIRECTIONS) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            if (ni >= 0 && ni < rows && nj >= 0 && nj < cols) {
+              if (grid.get(ni).charAt(nj) == '@') {
+                neighborPapers++;
+              }
+            }
+          }
+
+          if (neighborPapers < 4) {
+            removedPapers.add(new Point(i, j));
+          }
+        }
+      }
+
+      if(removedPapers.isEmpty()) break;
+
+      accessiblePapers += removedPapers.size();
+
+      removedPapers.forEach(paper -> {
+        char[] row = grid.get(paper.x).toCharArray();
+        row[paper.y] = 'x';
+        grid.set(paper.x, new String(row));
+      });
+
+    }
+
+    return accessiblePapers;
+  }
+
+  public static void main(String[] args) {
+    PrintingDepartment2 ex = new PrintingDepartment2();
+    System.out.println(ex.solution("input-sample.txt"));
+  }
+}
